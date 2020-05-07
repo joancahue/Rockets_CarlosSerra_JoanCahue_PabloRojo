861fd015e4e9eb5cb0f27fe7c8fdaf30041d2f25
projo <projo@192.168.1.140>
2020-05-07 09:09:08 +0200

    creacio main

--- Rockets_CarlosSerra_JoanCahue_PabloRojo/src/domini/Circuit.java
+++ Rockets_CarlosSerra_JoanCahue_PabloRojo/src/domini/Circuit.java
@@ -28,21 +28,5 @@ public class Circuit {
 		
 	}
 
-	public static void Main(String [] args) {
-		
-		Circuit c = new Circuit(800, 10);
-		int time = 1;
-		int maxTime = 10;
-				
-		
-		Rocket rocket = new Rocket();
-		
-		System.out.print("Starting competition. Circuit length: 800 Max time: 10");
-		
-		while(time > maxTime) {
-			
-			nextMove(time);
-			
-		}
-	}
 }
+
